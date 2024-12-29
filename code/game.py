a5faabc87a1c8c44eaca2057fb52c4d858db8ec6
Mayank <maku@makuchaku.in>
2024-12-29 09:05:35 -0800

    working car and road dividers

--- code/game.py
+++ code/game.py
@@ -5,44 +5,36 @@ from divider import *
 import random
 import time
 
-
 # width_or_height = 500
 screen_width = 800
 screen_height = 600
-pygame.font.init()
 
-
-line_image = pygame.image.load('../assets/line.png')
-line_1_x = 400
-line_1_y = 20
-line_1_change = 1
-
-
-
-
-window = pygame.display.set_mode((screen_width, screen_height))
-window.fill('light blue')
+sprite_box = 64
+speed = 1000
+num_sprites = 5
 
 tanks = []
 rockets = []
 dividers = []
-cars = []
-speed = 1000
-num_sprites = 5
 
 
 class Game:
 
     def __init__(self):
         self.energy_released = 0
-        # self.explosion_sound = pygame.mixer.music.load("explosion.wav")
+
+        # Init pygame
+        pygame.font.init()
+        window = pygame.display.set_mode((screen_width, screen_height))
+        window.fill('light blue')
+
+        # Init sound
         pygame.mixer.init()
         self.car = None
 
     # Creates all sprites
     def create_sprites(self):
-        self.car = Car(self, 500, 300, 64, 1)
-        # self.car.load()
+        self.car = Car(self, (screen_width-sprite_box)/2, (screen_height-sprite_box), sprite_box, 1)
         
         for i in range(3):
             divider1 = Divider(self, 400, 20, 64, i)
@@ -68,6 +60,10 @@ class Game:
             if event.type == pygame.QUIT:
                 break
 
+            # Cleans the screen
+            ## ONLY DO THIS BEFORE RENDERING ALL SPRITES
+            window.fill('light blue')
+
             # Set up all positions
             # self.car.move_y(-1)
             # time.sleep(1/speed)
@@ -77,33 +73,30 @@ class Game:
                 divider.move_y(1)
                 time.sleep(1/speed)
 
-            # Cleans the screen
-            window.fill('light blue')
-
             # Render dividers
             for divider in dividers:
                 divider.draw_divider(divider.sprite_x, divider.sprite_y)
 
-            # Checks for collisions
-            all_sprites = tanks + rockets
-            for sprite in all_sprites:
-                for other_sprite in all_sprites:
-                    if sprite.name != other_sprite.name:
-                        if sprite.check_and_change_direction(other_sprite) == True:
-                            if sprite.type == other_sprite.type:
-                                if sprite.killed == False and other_sprite.killed == False:
-                                    sprite.energy += 1
-                                    other_sprite.energy += 1
-                                    self.play_sound('../assets/punch.wav')
-                                    print('Sprite Energy', sprite.energy, 'Other Sprite Energy', other_sprite.energy)
-                            elif sprite.type != other_sprite.type:
-                                if sprite.killed == False and other_sprite.killed == False:
-                                    self.energy_released += (sprite.energy + other_sprite.energy)
-                                    sprite.kill_sprite()
-                                    other_sprite.kill_sprite()
-                                    # pygame.mixer.music.load("explosion.wav")
-                                    # pygame.mixer.music.play(loops=1)
-                                    self.play_sound('../assets/explosion.wav')
+            # # Checks for collisions
+            # all_sprites = tanks + rockets
+            # for sprite in all_sprites:
+            #     for other_sprite in all_sprites:
+            #         if sprite.name != other_sprite.name:
+            #             if sprite.check_and_change_direction(other_sprite) == True:
+            #                 if sprite.type == other_sprite.type:
+            #                     if sprite.killed == False and other_sprite.killed == False:
+            #                         sprite.energy += 1
+            #                         other_sprite.energy += 1
+            #                         self.play_sound('../assets/punch.wav')
+            #                         print('Sprite Energy', sprite.energy, 'Other Sprite Energy', other_sprite.energy)
+            #                 elif sprite.type != other_sprite.type:
+            #                     if sprite.killed == False and other_sprite.killed == False:
+            #                         self.energy_released += (sprite.energy + other_sprite.energy)
+            #                         sprite.kill_sprite()
+            #                         other_sprite.kill_sprite()
+            #                         # pygame.mixer.music.load("explosion.wav")
+            #                         # pygame.mixer.music.play(loops=1)
+            #                         self.play_sound('../assets/explosion.wav')
                         
             for divider in dividers:
                 if divider.sprite_y == screen_height:
